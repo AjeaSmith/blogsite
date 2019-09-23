3416b5ac39d7ebe660f9bbaf03169ab4c4b53627
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2019-09-23 16:26:28 -0400

    homepage outline

--- components/nav.js
+++ components/nav.js
@@ -54,17 +54,14 @@ const Nav = () => (
         </div>
       </div>
     </nav>
-    <header className="masthead" style={{ backgroundImage: "url('../static/post-sample-image.jpg')"}}>
+    <header className="masthead" style={{ backgroundImage: "url('../static/post-bg.jpg')"}}>
       <div className="overlay"></div>
       <div className="container">
         <div className="row">
           <div className="col-lg-8 col-md-10 mx-auto">
-            <div className="post-heading">
-              <h1>Man must explore, and this is exploration at its greatest</h1>
-              <h2 className="subheading">
-                Problems look mighty small from 150 miles up
-              </h2>
-              <span className="meta">
+            <div className="site-heading">
+              <h1>Awesome Blog</h1>
+              <span className="subheading">
                 Posted by
                 <a href="#">Start Bootstrap</a>
                 on August 24, 2019
